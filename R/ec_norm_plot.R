f552114069fc9455b6112fcceb4c1749888670e8
anders-kolstad <anders-lk@hotmail.com>
2026-05-07 20:38:05 +0200

    fix examples

--- R/ec_norm_plot.R
+++ R/ec_norm_plot.R
@@ -3,14 +3,14 @@
 #' Plot normalised ecological indicator values against the raw variable values
 #'
 #' Creates a scatter plot comparing raw variable values and corresponding
-#' normalised indicator values produced by [ec_normalise_dev()].
+#' normalised indicator values produced by `ec_normalise_dev()`.
 #'
 #' The plot is intended as a simple diagnostic and visualisation tool for
 #' evaluating ecological normalisation functions.
 #'
 #' @param variable Numeric vector of raw variable values.
 #' @param indicator Numeric vector of normalised indicator values, typically
-#'   produced by [ec_normalise_dev()].
+#'   produced by `ec_normalise_dev()`.
 #' @param lan Character. Plot language. One of `"english"` or `"norsk"`.
 #'   Defaults to `"english"`.
 #'
@@ -24,41 +24,41 @@
 #' @return A `ggplot2` object.
 #'
 #' @import ggplot2
-#' @importFrom ggridges geom_density_ridges_gradient
 #' @importFrom tibble tibble
 #' @export
 #'
 #' @examples
-#' x <- c(0, 2, 4, 6, 8, 10)
+#' x <- seq(0, 1000, by = 2)
 #'
-#' ind <- ec_normalise_dev(
+#' ind <- ec_normalise(
 #'   variable = x,
-#'   x0 = 0,
-#'   x100 = 10
+#'   x0 = 100,
+#'   x100 = 900,
+#'   x60 = 200
+#' )
+#'
+#' ec_norm_plot(
+#'   variable = x,
+#'   indicator = ind,
+#'   lan = "norsk"
+#' )
+#'
+#' ind <- ec_normalise(
+#'   variable = x,
+#'   x0 = 50,
+#'   x100 = rnorm(n = length(x), 400, 50),
+#'   x0h = 900,
+#'   fun = "sigmoid"
 #' )
 #'
 #' ec_norm_plot(
 #'   variable = x,
 #'   indicator = ind
 #' )
-#'
-#' ind_pw <- ec_normalise_dev(
-#'   variable = x,
-#'   x0 = 0,
-#'   x60 = 5,
-#'   x100 = 10
-#' )
-#'
-#' ec_norm_plot(
-#'   variable = x,
-#'   indicator = ind_pw,
-#'   lan = "norsk"
-#' )
 
 ec_norm_plot <- function(variable, indicator, lan = "english") {
   low <- "red"
   high <- "green"
-  ridge <- ggridges::geom_density_ridges_gradient(scale = 3, size = .3)
   mySize <- 8
   myAlpha <- .7
   myShape <- 21
